71dd4a1ee04362491da7001ebfbd2b87c15b8a32
qiaoyansong <qiaoyansong@didiglobal.com>
2022-01-25 14:41:15 +0800

    init

---
+++ common/src/main/java/com/util/HttpService.java
@@ -0,0 +1,12 @@
+package com.util;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author ：Qiao Yansong
+ * @date ：Created in 2022/1/25 2:23 下午
+ * description：用于封装HTTP调用服务
+ */
+@Component
+public class HttpService {
+}
---
+++ common/src/main/java/com/util/LogFactory.java
@@ -0,0 +1,9 @@
+package com.util;
+
+/**
+ * @author ：Qiao Yansong
+ * @date ：Created in 2022/1/25 2:33 下午
+ * description：日志管理工具类
+ */
+public class LogFactory {
+}
